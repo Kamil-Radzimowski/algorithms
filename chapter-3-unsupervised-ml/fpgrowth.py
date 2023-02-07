4d7367e2263533254db2ebb5d37e52620a3f02a9
kamil_radzimowski <k.radzimowski.736@studms.ug.edu.pl>
2023-02-07 20:50:58 +0100

    supervised ml

---
+++ chapter-3-unsupervised-ml/fpgrowth.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import pyfpgrowth as fp
+
+dict1 = {
+    'id': [0, 1, 2, 3],
+    'items': [['bramki', 'ochraniacze'],
+              ['kij', 'ochraniacze', 'kask'],
+              ['kask', 'ochraniacze'],
+              ['kij', 'ochraniacze', 'kask']
+              ]
+}
+
+transactionSet = pd.DataFrame(dict1)
+
+patterns = fp.find_frequent_patterns(transactionSet['items'], 1)
+print(patterns)
+
+rules = fp.generate_association_rules(patterns, 0.3)
+print(rules)
